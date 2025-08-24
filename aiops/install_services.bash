6333c26ce10f80deeea7cb92499363b46a8c6be2
randomdevice <stdrandomdevice@gmail.com>
2025-08-23 22:55:57 -0400

    chore: Updated config at Sat Aug 23 10:55:57 PM EDT 2025

--- aiops/install_services.bash
+++ aiops/install_services.bash
@@ -18,16 +18,16 @@ systemctl --user daemon-reload
 systemctl start --user ollama.service
 systemctl start --user comfy-ui
 systemctl start --user qdrant
-systemctl start --user n8n-postgres
-systemctl start --user n8n
+#systemctl start --user n8n-postgres
+#systemctl start --user n8n
 systemctl start --user kokoro-speech.service
 systemctl start --user openweb-ui
 
 systemctl enable --user ollama.service
 systemctl enable --user comfy-ui
 systemctl enable --user qdrant
-systemctl enable --user n8n-postgres
-systemctl enable --user n8n
+#systemctl enable --user n8n-postgres
+#systemctl enable --user n8n
 systemctl enable --user kokoro-speech.service
 systemctl enable --user openweb-ui
 
